c67c72ae11b35019543e42099d7c4506001e723d
baugod <bautistagodoy060@gmail.com>
2022-12-28 01:15:34 -0300

    a

--- PI-Food-main/api/src/controller/controller.js
+++ PI-Food-main/api/src/controller/controller.js
@@ -56,29 +56,34 @@ const getForName = async (req, res, next) => {
 const getRecipeForId = async (req, res)=> {
   const {id} = req.params;
  // const allRecipes = await getAllRecipes();
-  let validator = id.includes("-");
+ // let validator = id.includes("-");
+ const apiInfo = await getApiInfo();
+ const dbInfo = await getDBinfo();
+ const allInfo = apiInfo.concat(dbInfo);
+ let recipeId = await allInfo.find((e)=> e.id == id);
+ return res.status(200).json(recipeId);
 
-  if(validator){
-  try {
-      let idDb = await Recipe.findByPk(id, {includes: Typediet});
-      res.status(200).json([idDb]);
-  } 
-  catch(err) {
-      console.log(err);
-  }
-}
-else {
-  try{
-      if(id) {
-          let recipeId = await allRecipes.filter((e)=> e.id === parseInt(id));
-          recipeId.length
-          ?res.status(200).send(recipeId)
-          :res.status(400).send("Not found");
-      }
-  } catch(err) {
-      res.json({message: err});
-  }
-}
+ // if(validator){
+ // try {
+ //     let idDb = await Recipe.findByPk(id, {includes: Typediet});
+ //     res.status(200).json([idDb]);
+ // } 
+ // catch(err) {
+ //     console.log(err);
+ // }
+//}
+//else {
+//  try{
+ //     if(id) {
+  //        let recipeId = await allRecipes.filter((e)=> e.id === parseInt(id));
+  //        recipeId.length
+  //        ?res.status(200).send(recipeId)
+  //        :res.status(400).send("Not found");
+  //    }
+//  } catch(err) {
+  //    res.json({message: err});
+//  }
+//}
 };
 
 const getAllRecipes = async (req, res) => {
